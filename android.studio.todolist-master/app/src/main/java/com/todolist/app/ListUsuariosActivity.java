d4390631f254867025f93cc866973b5554e719c5
joelcn <joeltw2010@hotmail.com>
2016-12-01 08:35:40 -0200

    comentários das alterações

---
+++ android.studio.todolist-master/app/src/main/java/com/todolist/app/ListUsuariosActivity.java
@@ -0,0 +1,105 @@
+package com.todolist.app;
+
+/**
+ * Created by rafaelsinosaki on 21/09/16.
+ */
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.List;
+
+import adapter.UsuarioAdapter;
+import dao.UsuarioDAO;
+import model.Usuario;
+import util.Mensagem;
+
+
+public class ListUsuariosActivity extends ActionBarActivity implements
+        AdapterView.OnItemClickListener, DialogInterface.OnClickListener{
+
+    private ListView lista;
+    private List<Usuario> usuarioList;
+    private UsuarioAdapter usuarioAdapter;
+    private UsuarioDAO usuarioDAO;
+
+    private int idposicao;
+
+    private AlertDialog alertDialog, alertConfirmacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_usuarios);
+
+        alertDialog      = Mensagem.criarAlertDialog(this);
+        alertConfirmacao = Mensagem.criarDialogConfirmacao(this);
+
+        usuarioDAO     = new UsuarioDAO(this);
+        usuarioList    = usuarioDAO.listarUsuarios();
+        usuarioAdapter = new UsuarioAdapter(this, usuarioList);
+
+        lista = (ListView) findViewById(R.id.lvUsuarios);
+        lista.setAdapter(usuarioAdapter);
+
+        lista.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list_usuarios, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_cadastrar_usuario) {
+            startActivity(new Intent(this, CadUsuarioActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        int id = usuarioList.get(idposicao).get_id();
+
+        switch (which){
+            case 0:
+                Intent intent = new Intent(this, CadUsuarioActivity.class);
+                intent.putExtra("USUARIO_ID", id);
+                startActivity(intent);
+                break;
+            case 1:
+                alertConfirmacao.show();
+                break;
+            case DialogInterface.BUTTON_POSITIVE:
+                usuarioList.remove(idposicao);
+                usuarioDAO.removerUsuario(id);
+                lista.invalidateViews();
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                alertConfirmacao.dismiss();
+                break;
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        idposicao = position;
+        alertDialog.show();
+    }
+}
---
+++ android.studio.todolist-master/app/src/main/java/com/todolist/app/MainActivity.java
@@ -0,0 +1,61 @@
+package com.todolist.app;
+
+/**
+ * Created by rafaelsinosaki on 21/09/16.
+ */
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import util.Mensagem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_lista_usuarios:
+                startActivity(new Intent(this, ListUsuariosActivity.class));
+                break;
+            case R.id.action_lista_sair:
+                Mensagem.MsgConfirm(this, "Sair do sistema", "Deseja realmente sair?", R.drawable.ic_action_about, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                break;
+            case R.id.action_lista_logout:
+                SharedPreferences preferences   = getSharedPreferences("LoginActivityPreferences", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.commit();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
